0ed2652f525c8d902fb18342dcadf1c2f44379f6
JoseAlba <josealba1996@hotmail.com>
2017-11-02 23:30:58 -0400

    Paint half-way

---
+++ scripts/paint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paint : MonoBehaviour {
+
+	public GameObject sphere;
+
+	Ray myray;
+	RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		myray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if(Physics.Raycast(myray, out hit)){
+			//left mouse button is down
+			if(Input.GetKey(KeyCode.Mouse0)){
+				Instantiate (sphere, hit.point, Quaternion.identity);
+				Debug.Log (hit.point);
+			}
+		}
+	}
+}
